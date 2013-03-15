3d17a9d073cedae51a5642416405e87ca2b8f03f
Raimar Sandner <raimar.sandner@uibk.ac.at>
2013-03-15 10:43:59 +0100

    support writing of binary sv files to use with C++QED

--- pycppqed/iobin.cc
+++ pycppqed/iobin.cc
@@ -1,11 +1,14 @@
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <Python.h>
 #include <numpy/arrayobject.h>
 #include <blitz/array.h>
 #include <blitz/tinyvec2.h>
 #include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
 #include <boost/serialization/complex.hpp>
 #include <boost/preprocessor/iteration/local.hpp>
 #include <sstream>
+#include <fstream>
 #include <string>
 #include <iostream>
 
@@ -15,47 +18,84 @@ typedef std::complex<double> dcomp;
 
 using namespace blitz;
 
+template<int RANK>
+PyObject *doParse(boost::archive::binary_iarchive &ia) {
+  npy_intp dims[RANK];
+  Array<dcomp,RANK> a;
+  double t,dtTry;
+  for (int i=0;i<RANK;i++) { int d; ia>>d; dims[i]=d; }
+  ia >> a;
+  ia >> t >> dtTry;
+  dcomp *data = a.dataFirst();
+  PyObject *wrapNumpy = PyArray_SimpleNewFromData(RANK, dims, NPY_CDOUBLE,data);
+  PyArrayObject *array = (PyArrayObject *) PyArray_FromAny(wrapNumpy,NULL,0,0,NPY_ARRAY_ENSURECOPY,NULL);
+  return Py_BuildValue("(Odd)", PyArray_Return(array), t, dtTry);
+}
+
 static PyObject *parse(PyObject *self, PyObject *args){
     // Read in arguments: datastr, length
     char *datastr;
-    int size,d;
+    int size;
     double t,dtTry;
     if (!PyArg_ParseTuple(args, "s#", &datastr, &size)) return NULL;
-    
-    std::complex<double> *data;
-    int rank=1;
-    npy_intp *dims;
+    int rank;
     std::stringstream datastream(std::string(datastr, size));
-
-    #define BOOST_PP_LOCAL_MACRO(n) Array<dcomp,n> a##n;
-    #define BOOST_PP_LOCAL_LIMITS (1, MAX_RANK)
-    #include BOOST_PP_LOCAL_ITERATE()
-
     boost::archive::binary_iarchive ia(datastream);
     ia >> rank;
     if (rank>MAX_RANK) {
         std::cerr << "Rank > " << MAX_RANK << " not supported." << std::endl;
         return NULL;
     }
-    dims = new npy_intp[rank];
-    for (int i=0;i<rank;i++) { ia>>d; dims[i]=d; }
+
     switch (rank) {
         #define BOOST_PP_LOCAL_MACRO(n) \
-          case n: ia >> a##n; data = a##n.dataFirst(); break; \
-        /**/
+          case n: return doParse<n>(ia); break;
         #define BOOST_PP_LOCAL_LIMITS (1, MAX_RANK)
         #include BOOST_PP_LOCAL_ITERATE()
     }
-    ia>>t>>dtTry;
-    PyObject *wrapNumpy = PyArray_SimpleNewFromData(rank, dims, PyArray_CDOUBLE,data);
-    PyArrayObject *array = (PyArrayObject *) PyArray_FromAny(wrapNumpy,NULL,0,0,NPY_ENSURECOPY,NULL);
-    delete[] dims;
-    return Py_BuildValue("(Odd)", PyArray_Return(array), t, dtTry);
-    }
+}
 
+template<int RANK>
+void doWrite(boost::archive::binary_oarchive &oa, PyArrayObject *array_c, const double time) {
+  npy_intp *dims=PyArray_DIMS(array_c);
+  TinyVector<int,RANK> shape;
+  for (int i=0; i<RANK; i++) {
+    int d=dims[i];
+    oa << d;
+    shape[i]=d;
+  }
+  Array<dcomp,RANK>  a = Array<dcomp,RANK>(static_cast<dcomp *>(PyArray_DATA(array_c)), shape, duplicateData);
+  const double dtTry=1;
+  oa << a << time << dtTry;
+}
+
+static PyObject *write(PyObject *self, PyObject *args){
+  char *filename;
+  PyObject *array;
+  double time;
+  if (!PyArg_ParseTuple(args, "sO!d", &filename, &PyArray_Type, &array, &time)) return NULL;
+  PyArrayObject *array_c = (PyArrayObject *) PyArray_FromAny(array, PyArray_DescrFromType(NPY_CDOUBLE),0,0,NPY_ARRAY_CARRAY_RO,NULL);
+  std::ofstream ofs(filename,ios_base::out|ios_base::binary|ios_base::trunc);
+  boost::archive::binary_oarchive oa(ofs);
+  int r=PyArray_NDIM(array_c);
+  if (r>MAX_RANK) {
+    std::cerr << "Rank > " << MAX_RANK << " not supported." << std::endl;
+    return NULL;
+  }
+  oa << r;
+
+  switch (r) {
+    #define BOOST_PP_LOCAL_MACRO(n)               \
+      case n: doWrite<n>(oa,array_c,time); break;
+    #define BOOST_PP_LOCAL_LIMITS (1, MAX_RANK)
+    #include BOOST_PP_LOCAL_ITERATE()
+  }
+  Py_RETURN_TRUE;
+}
 
 static PyMethodDef DataMethods[] = {
     {"parse", parse, METH_VARARGS, "Parse binary blitz array into numpy array."},
+    {"write", write, METH_VARARGS, "Write numpy array into binary file usable as C++QED initial file."},
     {NULL, NULL, 0, NULL},
     };
 
